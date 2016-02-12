302830bb7955b151d8c66fe4123a05d9cb16975f
Francesco Balestrieri <francesco.balestrieri@intel.com>
2016-02-12 08:00:15 +0200

    Add tasks to minify and inline

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var htmlmin = require('gulp-htmlmin');
+var inlinesource = require('gulp-inline-source');
+var del = require('del');
 
-// Static server
 gulp.task('serve', function () {
   browserSync.init({
     server: {
@@ -12,3 +14,14 @@ gulp.task('serve', function () {
   gulp.watch(['app/styles/*.css'], browserSync.reload);
   gulp.watch(['app/scripts/*.js'], browserSync.reload);
 });
+
+gulp.task('inline', function() {
+  return gulp.src('app/*.html')
+    .pipe(inlinesource({compress: false}))
+    .pipe(htmlmin({collapseWhitespace: true, minifyJS: true, minifyCSS: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+  del(['dist']);
+})
