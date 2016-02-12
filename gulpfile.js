03b58c8a9224763c61332047902c9c4687e0d267
Francesco Balestrieri <francesco.balestrieri@intel.com>
2016-02-12 11:25:43 +0200

    Use Sass for styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,27 @@ var browserSync = require('browser-sync').create();
 var htmlmin = require('gulp-htmlmin');
 var inlinesource = require('gulp-inline-source');
 var del = require('del');
-
-gulp.task('serve', function () {
+var sass = require('gulp-sass');
+ 
+gulp.task('serve', ['sass'], function () {
   browserSync.init({
     server: {
       baseDir: "./app"
     }
   });
   gulp.watch(['app/*.html'], browserSync.reload);
-  gulp.watch(['app/styles/*.css'], browserSync.reload);
+  gulp.watch("app/styles/*.scss", ['sass']);
   gulp.watch(['app/scripts/*.js'], browserSync.reload);
 });
 
-gulp.task('inline', function() {
+gulp.task('sass', function () {
+  return gulp.src('app/styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/styles'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('dist', ['sass'], function() {
   return gulp.src('app/*.html')
     .pipe(inlinesource({compress: false}))
     .pipe(htmlmin({collapseWhitespace: true, minifyJS: true, minifyCSS: true}))
@@ -25,3 +33,5 @@ gulp.task('inline', function() {
 gulp.task('clean', function() {
   del(['dist']);
 })
+
+gulp.task('default', ['clean','inline']);
