d9066b8463f82f0740f268ce3affa12db52def97
Francesco Balestrieri <francesco.balestrieri@intel.com>
2016-03-02 13:57:36 +0200

    Add hydration setting

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,29 @@
 (function () {
   'use strict';
 
-  var app = {
-    hydration: 0.70,
+  var dough = {
+    hydration: 70,
     yeastPrc: 0.003,
     saltPrc: 0.015,
-    weightPerPizza: 225
+    weightPerPizza: 225,
+    pizzas: 4,
+    flour: 0,
+    water: 0,
+    salt: 0,
+    yeast: 0
   };
+  
+  dough.update = function() {
+    
+    dough.flour = Math.round(dough.pizzas * dough.weightPerPizza / (1 + dough.hydration/100));
+    dough.water = dough.pizzas * dough.weightPerPizza - dough.flour;
+    dough.salt = Math.round(dough.pizzas * dough.weightPerPizza * dough.saltPrc);
+    dough.yeast = Math.round(dough.pizzas * dough.weightPerPizza * dough.yeastPrc);
+  }
+  
+  dough.update();
+  
+  var app = {};
 
   app.updateIngredients = function () {
     
@@ -15,32 +32,44 @@
     if (!pizzas) 
       return;
     
+    dough.pizzas = pizzas;
     localStorage.numberOfPizzas = pizzas;
     
-    var flour = Math.round(pizzas * this.weightPerPizza / (1 + this.hydration));
-    var water = pizzas * this.weightPerPizza - flour;
-    var salt = Math.round(pizzas * this.weightPerPizza * this.saltPrc);
-    var yeast = Math.round(pizzas * this.weightPerPizza * this.yeastPrc);
+    dough.hydration = document.getElementById('hydration-input').value;
+    localStorage.hydration = dough.hydration;
     
-    document.getElementById('flour').innerHTML = flour.toString() + "g";
-    document.getElementById('water').innerHTML = water.toString() + "g";
-    document.getElementById('salt').innerHTML = salt.toString() + "g";
-    document.getElementById('yeast').innerHTML = yeast.toString() + "g";
+    dough.update();
+    
+    document.getElementById('flour').innerHTML = dough.flour.toString() + "g";
+    document.getElementById('water').innerHTML = dough.water.toString() + "g";
+    document.getElementById('salt').innerHTML = dough.salt.toString() + "g";
+    document.getElementById('yeast').innerHTML = dough.yeast.toString() + "g";
     
   }
   
   app.restoreData = function () {
-     var pizzas = localStorage.numberOfPizzas;
-     document.getElementById('pizzas').value = pizzas ? pizzas : 4;
+     dough.pizzas = localStorage.numberOfPizzas;
+     document.getElementById('pizzas').value = dough.pizzas ? dough.pizzas : 4;
+     dough.hydration = localStorage.hydration;
+     document.getElementById('hydration-input').value = dough.hydration ? dough.hydration : 65;
+     document.getElementById('hydration-value').innerHTML = dough.hydration + "%";
      this.updateIngredients();
   }
-  
+      
   document.getElementById('pizzas').addEventListener('input', function(){ app.updateIngredients(); });
+  
   document.getElementById('pizzas').addEventListener('focusout', 
     function(){ 
-      if (!document.getElementById('pizzas').value)
+      if (!this.value)
         app.restoreData(); 
     });
+  
+  var hi = document.getElementById('hydration-input');
+  hi.addEventListener('input', function() {
+    document.getElementById('hydration-value').innerHTML = this.value + "%";  
+  });
+  hi.addEventListener('input', app.updateIngredients);
+  hi.addEventListener('change', app.updateIngredients);
   
   // Init app
   app.restoreData();
